23965cbf9c08f9879013810a0f783a0807ee3a6b
Mohanraj <93506428+Mr-Mohanraj@users.noreply.github.com>
2023-01-30 16:57:53 +0530

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
