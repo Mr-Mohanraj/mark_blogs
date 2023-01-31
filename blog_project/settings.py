79d493227de49c5cdd27fd878c83911e8b0c2d9b
Mohanraj <170602mark@gmail.com>
2023-01-31 11:12:40 +0530

    static file problem sloved

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -162,9 +162,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'),)
-
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
